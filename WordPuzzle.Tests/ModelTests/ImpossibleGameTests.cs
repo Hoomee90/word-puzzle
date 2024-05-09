fca38d477c6017c74765caa4e738a396f2820d60
Samantha Callie <Hoomee90@gmail.com>
2024-05-09 14:19:32 -0700

    make impossible game class

---
+++ WordPuzzle.Tests/ModelTests/ImpossibleGameTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordPuzzle.Models;
+
+namespace WordPuzzle.Tests
+{
+	[TestClass]
+	public class ImpossibleGameTests
+	{
+		[TestMethod]
+		public void RevealAnswer_ReturnsWordOptionNotGuessed_String()
+		{
+			string expected = "banana";
+			ImpossibleGame newGame = new();
+			newGame.Guess('e');
+			newGame.Guess('y');
+			newGame.Guess('i');
+			newGame.Guess('o');
+			newGame.Guess('u');
+			Assert.AreEqual(expected, newGame.GetAnswer());
+		}
+	}
+}
---
+++ WordPuzzle/Models/ImpossibleGame.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace WordPuzzle.Models
+{
+	public class ImpossibleGame : Game
+	{
+		public string[] WordOptions { get; } = { "banana", "excess", "giggit", "voodoo", "pullup", "myrrhy" };
+		
+		public ImpossibleGame() : base("")
+		{
+			GuessesLeft = 5;
+		}
+		
+		public override bool Guess(char letter)
+		{
+			GuessedLetters.Add(letter);
+			GuessesLeft--;
+			return false;
+		}
+		
+		public override string GetAnswer()
+		{
+			return Array.Find(WordOptions, el => !GuessedLetters.Exists(ch => el.Contains(ch)));
+		}
+	}
+}
